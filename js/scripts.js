e330c5ca1a4eb3d85c83c22585b48c5c35b37960
Denis Tutov <vrednokot@gmail.com>
2019-10-14 13:28:26 +0300

    end

--- js/scripts.js
+++ js/scripts.js
@@ -6,6 +6,21 @@ var closeWriteForm = document.querySelector('.write-close-btn');
 var userNameField = document.querySelector('#user-name');
 var emailField = document.querySelector('#user-email');
 
+var KEYCODE_ESC = 27;
+
+window.addEventListener('keydown', function (evt) {
+  if (evt.keyCode === KEYCODE_ESC) {
+    if (popupWriteForm.classList.contains('modal-show')) {
+      evt.preventDefault();
+      popupWriteForm.classList.remove('modal-show');
+    }
+    if (popupMap.classList.contains('modal-show')) {
+      evt.preventDefault();
+      popupMap.classList.remove('modal-show');
+    }
+  }
+});
+
 openWriteForm.addEventListener('click', function (evt) {
   evt.preventDefault();
   popupWriteForm.classList.add('modal-show');
@@ -25,23 +40,34 @@ closeWriteForm.addEventListener('click', function (evt) {
   popupWriteForm.classList.remove('modal-error');
 });
 
+popupWriteForm.addEventListener('click', function (evt) {
+  evt.preventDefault();
+  popupWriteForm.classList.remove('modal-show');
+  popupWriteForm.classList.remove('modal-error');
+});
+
 // Contacts map modal
 
 var openMap = document.querySelector('.contacts-map');
 var closeMap = document.querySelector('.map-close-btn');
 var popupMap = document.querySelector('.modal-map');
 
-  openMap.addEventListener('click', function (evt) {
-    evt.preventDefault(evt);
-    popupMap.classList.add('modal-show');
+openMap.addEventListener('click', function (evt) {
+  evt.preventDefault(evt);
+  popupMap.classList.add('modal-show');
 });
 
-  closeMap.addEventListener('click', function (evt) {
-    evt.preventDefault(evt);
-    popupMap.classList.remove('modal-show');
+closeMap.addEventListener('click', function (evt) {
+  evt.preventDefault(evt);
+  popupMap.classList.remove('modal-show');
 });
 
-// services slider
+popupMap.addEventListener('click', function (evt) {
+  evt.preventDefault(evt);
+  popupMap.classList.remove('modal-show');
+});
+
+// Services slider
 
 var servicesButtons = Array.prototype.slice.call(document.querySelectorAll('.services-btn'));
 var services = document.querySelector('.services-content');
@@ -60,70 +86,54 @@ servicesButtons.forEach(function (button, i) {
   });
 });
 
-// Modal add to cart
-
-var openCatalogBuyForm = document.querySelector('.cart-item-add');
-var closeCatalogBuyForm = document.querySelector('.cart-close-btn');
-var catalogBuyButtons = document.querySelectorAll('.image-hover-btn');
-var continueShoppingBtn = document.querySelector('.cart-item-back__btn');
-
-closeCatalogBuyForm.addEventListener('click', function () {
-  openCatalogBuyForm.classList.remove('modal-show');
-});
-
-continueShoppingBtn.addEventListener('click', function () {
-  openCatalogBuyForm.classList.remove('modal-show');
-});
-
 // Slider offer
 
 var sliderArray = document.querySelectorAll('.slider-block');
-var sliderWrapper = document.querySelector('.offers-slider-wrapper');
 var sliderMoveRightButton = document.querySelector('.controls__right-btn');
 var sliderMoveLeftButton = document.querySelector('.controls__left-btn');
 var sliderDots = document.querySelectorAll('.offers-slider-dot');
-var moveDistance = 270;
-var currentSlidePosition = 0;
-var sliderCounter = 1;
 
+var sliderCounter = 0;
 var slideCount = sliderArray.length;
 
-var moveSlider = function (distance) {
-  return sliderWrapper.style.transform = 'translateY(' + distance +'px)';
-};
-
 var makeDefaultDot = function (number) {
-  --number;
   sliderDots[number].classList.remove('dot-current');
 };
 
 var makeCurrentDot = function (number) {
-  --number;
   sliderDots[number].classList.add('dot-current');
 };
 
+var showNextSLide = function (number) {
+  sliderArray[number].classList.add('visually-hidden');
+  ++number;
+  sliderArray[number].classList.remove('visually-hidden');
+};
+
+var showPreviousSLide = function (number) {
+  sliderArray[number].classList.add('visually-hidden');
+  --number;
+  sliderArray[number].classList.remove('visually-hidden');
+};
+
 sliderMoveRightButton.addEventListener('click', function (evt) {
-  if (slideCount > sliderCounter) {
-    currentSlidePosition -= moveDistance;
-    moveSlider(currentSlidePosition);
+  if (slideCount > sliderCounter + 1) {
+    showNextSLide(sliderCounter);
     makeDefaultDot(sliderCounter);
     ++sliderCounter;
     makeCurrentDot(sliderCounter);
   } else {
     evt.preventDefault();
   }
-  console.log(sliderCounter);
 });
 
 sliderMoveLeftButton.addEventListener('click', function (evt) {
-  if (sliderCounter > 1) {
-    currentSlidePosition += moveDistance;
-    moveSlider(currentSlidePosition);
+  if (sliderCounter > 0) {
+    showPreviousSLide(sliderCounter);
     makeDefaultDot(sliderCounter);
     --sliderCounter;
     makeCurrentDot(sliderCounter);
   } else {
     evt.preventDefault();
   }
-  console.log(sliderCounter);
 });
